4b6bc1488cb52318b4454c0a69ef4d6005507e96
fangwenrui <fangwenrui1997@outlook.com>
2022-11-30 14:37:21 -0800

    try fix bug2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = 'mongodb://localhost:27017/tuiter' || process.env.DB_CONNECTION_STRING
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors({
