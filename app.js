162ef010b6f95ccf0dff26c330f3a0b6eb548738
fangwenrui <fangwenrui1997@outlook.com>
2022-11-29 23:31:05 -0800

    try fix bug

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ import mongoose from "mongoose";
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: true
+}))
 app.use(express.json());
 const CONNECTION_STRING = 'mongodb://localhost:27017/tuiter' || process.env.DB_CONNECTION_STRING
 mongoose.connect(CONNECTION_STRING);
